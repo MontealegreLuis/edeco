3f462fb9d12acd7d0e78787556f946367bdb81ee
Luis Montealegre <montealegreluis@gmail.com>
2017-02-15 20:23:17 -0600

    Update the form factory class.
    * Add type hints and return types
    * Use aliases for shorter class names
    * Remove redundant doc blocks.

--- src/Mandragora/Form.php
+++ src/Mandragora/Form.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * PHP version 5
+ * PHP version 7.1
  *
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
 namespace Mandragora;
 
-use Zend_Cache_Core;
-use Zend_Filter_Word_CamelCaseToDash;
-use Zend_Config_Ini;
+use Mandragora\Form\SecureForm;
+use Zend_Cache_Core as Cache;
+use Zend_Filter_Word_CamelCaseToDash as CamelCaseToDash;
+use Zend_Config_Ini as IniConfig;
 
 /**
  * Factory for forms
@@ -29,78 +30,55 @@ class Form
      */
     protected $fromConfig;
 
-    /**
-     * @var Zend_Cache_Core
-     */
+    /** @var Cache */
     protected static $cache;
 
-    /**
-     * @param boolean $disableCache
-     * @param boolean $fromConfig
-     */
-    public function __construct($disableCache, $fromConfig)
+    public function __construct(bool $disableCache, bool $fromConfig)
     {
         $this->disableCache = $disableCache;
         $this->fromConfig = $fromConfig;
     }
 
     /**
-     * @param Zend_Cache_Core $cache
      * @return void
      */
-    public function setCache(Zend_Cache_Core $cache)
+    public function setCache(Cache $cache)
     {
         self::$cache = $cache;
     }
 
-    /**
-     * @param string $name
-     * @param string $model
-     * @return Mandragora_Form_Abstract
-     */
-    public function factory($name, $model)
+    public function factory(string $className, string $model): SecureForm
     {
         if ($this->fromConfig) {
-            return $this->fromConfig($name, $model);
+            return $this->fromConfig($className, $model);
         } else {
-            return $this->fromClass($name, $model);
+            return $this->fromClass($className, $model);
         }
     }
 
-    /**
-     * @param string $name
-     * @param string $model
-     * @return Mandragora_Form_Abstract
-     */
-    protected function fromClass($name, $model)
+    protected function fromClass(string $name, string $model): SecureForm
     {
         $className = $this->getClassName($name, $model);
         return new $className();
     }
 
-    /**
-     * @param string $name
-     * @param string $model
-     * @return Mandragora_Form_Abstract
-     */
-    protected function fromConfig($name, $model)
+    protected function fromConfig(string $name, string $model): SecureForm
     {
         return $this->createFormFromConfig($name, $model);
     }
 
-    /**
-     * @param string $name
-     * @param string $model
-     */
-    protected function createFormFromConfig($name, $model)
+    protected function createFormFromConfig(
+        string $name,
+        string $model
+    ): SecureForm
     {
         $className = $this->getClassName($name, $model);
-        $filter = new Zend_Filter_Word_CamelCaseToDash();
+        $filter = new CamelCaseToDash();
         $folder = strtolower($filter->filter($model));
         $file = strtolower($filter->filter($name));
         if ($this->disableCache) {
             $form = $this->_loadFormFromIniFile($folder, $file, $className);
-        } else  {
+        } else {
             $cacheTag = str_replace('\\', '_', $className);
             $form = $this->getCache()->load($cacheTag);
             if (!$form) {
@@ -111,35 +89,27 @@ class Form
         return $form;
     }
 
-    /**
-     * @return Zend_Cache_Core
-     */
-    protected function getCache()
+    protected function getCache(): Cache
     {
         return self::$cache;
     }
 
-    /**
-     * @param string $formName
-     * @param string $model
-     * @return string
-     */
-    protected function getClassName($formName, $model)
+    protected function getClassName(string $formName, string $model): string
     {
         return sprintf('App\Form\%s\%s', $model, $formName);
     }
 
     /**
-     * @param string $folder
-     * @param string $fileName
-     * @param string $className
-     * @return Mandragora_Form_Abstract
+     * @throws \Zend_Config_Exception
      */
-    private function _loadFormFromIniFile($folder, $fileName, $className)
+    private function _loadFormFromIniFile(
+        string $folder,
+        string $fileName,
+        string $className
+    ): SecureForm
     {
         $pathToIniFile = APPLICATION_PATH . '/configs/forms/%s/%s.ini';
         $configPath = sprintf($pathToIniFile, $folder, $fileName);
-        $config = new Zend_Config_Ini($configPath, sprintf('%s', $folder));
-        return new $className($config);
+        return new $className(new IniConfig($configPath, sprintf('%s', $folder)));
     }
 }
