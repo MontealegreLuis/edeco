9fa7f534771b0d480b8acc780a348eb3e6058780
Luis Montealegre <montealegreluis@gmail.com>
2017-02-17 20:28:59 -0600

    Create tests for contact form.

---
+++ tests/unit/App/Form/Contact/DetailTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * PHP version 7.0
+ *
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+namespace App\Form\Contact;
+
+use Mandragora\FormFactory;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class DetailTest extends TestCase
+{
+    /** @test */
+    function it_can_be_created()
+    {
+        $this->assertInstanceOf(Detail::class, $this->contactForm);
+        $this->assertCount(5, $this->contactForm->getElements());
+    }
+
+    /** @before */
+    function createForm()
+    {
+        $this->contactForm = (new FormFactory(true))->create('Detail',
+            'Contact');
+    }
+
+    /** @var Detail */
+    private $contactForm;
+}
