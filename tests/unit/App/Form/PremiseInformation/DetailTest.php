8f02909605e464d07a25300412fe9233924ea882
Luis Montealegre <montealegreluis@gmail.com>
2017-02-21 13:38:52 -0600

    Add test for the premise information form.

---
+++ tests/unit/App/Form/PremiseInformation/DetailTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * PHP version 7.1
+ *
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+namespace App\Form\PremiseInformation;
+
+use Mandragora\FormFactory;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class DetailTest extends TestCase
+{
+    /** @test */
+    function it_can_be_created()
+    {
+        $this->assertInstanceOf(Detail::class, $this->premiseForm);
+        $this->assertCount(9, $this->premiseForm->getElements());
+    }
+
+    /** @before */
+    function createForm()
+    {
+        $this->premiseForm = (new FormFactory(true))->create('Detail', 'PremiseInformation');
+    }
+
+    /** @var Detail */
+    private $premiseForm;
+}
