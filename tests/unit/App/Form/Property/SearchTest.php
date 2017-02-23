5e10eeab132e1179ffc4a43a2f229c0664c8cef1
Luis Montealegre <montealegreluis@gmail.com>
2017-02-22 20:32:38 -0600

    Add tests for the search form.

---
+++ tests/unit/App/Form/Property/SearchTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * PHP version 7.1
+ *
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+namespace App\Form\Property;
+
+use Mandragora\FormFactory;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class SearchTest extends TestCase
+{
+    /** @test */
+    function it_can_be_created()
+    {
+        $this->assertInstanceOf(Search::class, $this->searchForm);
+        $this->assertCount(2, $this->searchForm->getElements());
+    }
+
+    /** @before */
+    function createForm()
+    {
+        $this->searchForm = (new FormFactory(true))->create('Search', 'Property');
+    }
+
+    /** @var Search */
+    private $searchForm;
+}
