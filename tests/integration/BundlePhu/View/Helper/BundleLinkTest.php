53cfc5bf2fa2b85d359de3c05b6d6fe4b5fdadac
Luis Montealegre <montealegreluis@gmail.com>
2017-06-09 21:29:17 -0500

    Use a CSS dummy file within the fixtures folder for tests

--- tests/integration/BundlePhu/View/Helper/BundleLinkTest.php
+++ tests/integration/BundlePhu/View/Helper/BundleLinkTest.php
@@ -17,14 +17,14 @@ class BundleLinkTest extends TestCase
     /** @test */
     function it_bundles_a_single_css_file()
     {
-        $this->bundleLink->appendStylesheet('/styles/breadcrumbs.css', 'screen, projection');
+        $this->bundleLink->appendStylesheet('/breadcrumbs.css', 'screen, projection');
 
         $link = $this->bundleLink->toString();
 
-        $this->assertRegExp('/<link href="\/styles\/min\/bundle\-admin\-address\-edit\-screen\.css\?\d{10}" media="screen, projection" rel="stylesheet" type="text\/css" \/>/', $link);
-        $this->assertRegExp('/<link href="\/styles\/min\/bundle\-admin\-address\-edit\-print\.css\?\d{10}" media="print" rel="stylesheet" type="text\/css" \/>/', $link);
-        $this->assertRegExp('/<!\-\-\[if IE\]><link rel="stylesheet" type="text\/css" media="screen, projection" href="\/styles\/min\/bundle\-admin\-address\-edit\-IE\.css\?\d{10}" \/><!\[endif\]\-\->/', $link);
-        $this->assertTrue(File::exists($this->bundleFile)); // there should be three files...
+        $this->assertRegExp('/<link href="\/min\/bundle\-admin\-address\-edit\-screen\.css\?\d{10}" media="screen, projection" rel="stylesheet" type="text\/css" \/>/', $link);
+        $this->assertRegExp('/<link href="\/min\/bundle\-admin\-address\-edit\-print\.css\?\d{10}" media="print" rel="stylesheet" type="text\/css" \/>/', $link);
+        $this->assertRegExp('/<!\-\-\[if IE\]><link rel="stylesheet" type="text\/css" media="screen, projection" href="\/min\/bundle\-admin\-address\-edit\-IE\.css\?\d{10}" \/><!\[endif\]\-\->/', $link);
+        $this->assertTrue(File::exists($this->bundleFile));
     }
 
     /** @before */
@@ -36,14 +36,11 @@ class BundleLinkTest extends TestCase
         $request->setModuleName('admin')->setControllerName('address')->setActionName('edit');
         $this->frontController->setRequest($request);
 
-        $publicPath = __DIR__ . '/../../../../../admin.edeco.mx';
-        $bundlePath = 'styles/min';
+        $publicPath = __DIR__ . '/../../../../../tests/fixtures';
+        $bundlePath = 'min';
         $this->bundleFile = "$publicPath/$bundlePath/bundle-admin-address-edit-screen.css";
 
         $this->bundleLink = new BundleLink();
-        $view = new View();
-        $view->setBasePath('/');
-
         $command = sprintf(
             'java -Xmx128m -jar %s :sourceFile -o :filename --charset utf-8',
             escapeshellarg(__DIR__ . '/../../../../../bin/yuicompressor-2.4.2.jar')
@@ -54,7 +51,7 @@ class BundleLinkTest extends TestCase
             ->setCacheDir(__DIR__ . '/../../../../../var/cache/js')
             ->setDocRoot($publicPath)
             ->setUrlPrefix($bundlePath)
-            ->setView($view)
+            ->setView(new View(['basePath' => '/']))
         ;
     }
 
@@ -63,9 +60,6 @@ class BundleLinkTest extends TestCase
 
     /** @var  FrontController */
     private $frontController;
-
-    /** @var TestRequest */
-    private $request;
 
     /** @var string */
     private $bundleFile;
